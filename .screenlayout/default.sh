3828de42ee9c55a61bca8418993928a5a81d9db4
fleetscut <fleetscut@gmail.com>
2019-11-03 20:06:33 -0500

    Adding xrandr script for monitor setup

---
+++ .screenlayout/default.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --mode 1920x1080 --pos 0x670 --rotate left --output HDMI-0 --mode 2560x1080 --pos 1574x0 --rotate normal --output DP-0 --primary --mode 3440x1440 --pos 1080x1080 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --mode 1920x1080 --pos 4520x600 --rotate right --output DP-4 --off --output DP-5 --off
