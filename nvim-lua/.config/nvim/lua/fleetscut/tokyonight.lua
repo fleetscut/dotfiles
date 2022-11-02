1b3c0a0f83361ec175726a42a7cf5adf47ebee89
fleetscut <fleetscut@gmail.com>
2022-11-02 15:57:44 -0400

    Tokyonight new config. Cmp depreceated function

--- nvim-lua/.config/nvim/lua/fleetscut/tokyonight.lua
+++ nvim-lua/.config/nvim/lua/fleetscut/tokyonight.lua
@@ -1,13 +1,18 @@
-vim.g.tokyonight_style = "night"
-vim.g.tokyonight_dark_float = false
-vim.g.tokyonight_transparent= true
-vim.g.tokyonight_colors = { bg_float = "none" }
-vim.cmd[[hi Normal guibg=none ctermbg=none]]
-vim.cmd[[hi LineNr guibg=none ctermbg=none]]
-vim.cmd[[hi Folded guibg=none ctermbg=none]]
-vim.cmd[[hi NonText guibg=none ctermbg=none]]
-vim.cmd[[hi SpecialKey guibg=none ctermbg=none]]
-vim.cmd[[hi VertSplit guibg=none ctermbg=none]]
-vim.cmd[[hi SignColumn guibg=none ctermbg=none]]
-vim.cmd[[hi EndOfBuffer guibg=none ctermbg=none]]
+require("tokyonight").setup({
+    style = "night",
+    transparent = true,
+})
+
+-- vim.g.tokyonight_style = "night"
+-- vim.g.tokyonight_dark_float = false
+-- vim.g.tokyonight_transparent= true
+-- vim.g.tokyonight_colors = { bg_float = "none" }
+-- vim.cmd[[hi Normal guibg=none ctermbg=none]]
+-- vim.cmd[[hi LineNr guibg=none ctermbg=none]]
+-- vim.cmd[[hi Folded guibg=none ctermbg=none]]
+-- vim.cmd[[hi NonText guibg=none ctermbg=none]]
+-- vim.cmd[[hi SpecialKey guibg=none ctermbg=none]]
+-- vim.cmd[[hi VertSplit guibg=none ctermbg=none]]
+-- vim.cmd[[hi SignColumn guibg=none ctermbg=none]]
+-- vim.cmd[[hi EndOfBuffer guibg=none ctermbg=none]]
 vim.cmd[[colorscheme tokyonight]]
