601801d0e8ece8495c3cefc6400c8643b89f1b75
yes <yes>
2023-10-24 15:01:06 -0400

    org stuff

--- nvim-2023/.config/nvim/lua/plugins/orgmode.lua
+++ nvim-2023/.config/nvim/lua/plugins/orgmode.lua
@@ -1,11 +1,31 @@
 return {
 	"nvim-orgmode/orgmode",
 	-- event = "VeryLazy",
+	enabled = true,
 	config = function()
-		require("orgmode").setup_ts_grammar()
 		require("orgmode").setup({
 			org_agenda_files = "~/.orgfiles/**/*",
 			org_default_notes_file = "~/.orgfiles/refile.org",
+			org_capture_templates = {
+				n = {
+					description = "Note",
+					template = "* %? :NOTE:\n\t%U\n\t",
+				},
+				a = "Nested",
+				aa = {
+					description = "Nest 1",
+					template = "* %? Nest 1",
+				},
+				ab = {
+					description = "Nest 2",
+					template = "* %? Nest 2",
+				},
+			},
+			mappings = {
+				capture = {
+					org_capture_finalize = "<Leader>w",
+				},
+			},
 		})
 	end,
 }
--- nvim-lua/.config/nvim/init.lua
+++ nvim-lua/.config/nvim/init.lua
@@ -1,1 +1,1 @@
-require('fleetscut')
+require("fleetscut")
