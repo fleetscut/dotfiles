22005c68de81a79168e927eee3943ff67a7c960c
Chris <fleetscut@gmail.com>
2024-06-01 23:32:23 -0400

    added hyprland config

--- nvim-2023/.config/nvim/lua/plugins/noice.lua
+++ nvim-2023/.config/nvim/lua/plugins/noice.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"folke/noice.nvim",
 		event = "VeryLazy",
-		enabled = false,
+		enabled = true,
 		dependencies = {
 			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 			"MunifTanjim/nui.nvim",
@@ -10,6 +10,54 @@ return {
 			--   `nvim-notify` is only needed, if you want to use the notification view.
 			--   If not available, we use `mini` as the fallback
 			"rcarriga/nvim-notify",
+		},
+		init = function()
+			require("fleetscut.keymaps").noice_keymap(bufnr)
+		end,
+		opts = {
+			cmdline = { enabled = true, view = "cmdline_popup" },
+			messages = {
+				enabled = true,
+			},
+
+			routes = {
+				{
+					filter = {
+						event = "msg_show",
+						find = "agenda command",
+					},
+					view = "popup",
+				},
+				{
+					filter = {
+						event = "msg_show",
+						find = "capture template",
+					},
+					view = "popup",
+				},
+				{
+					filter = {
+						event = "msg_show",
+						find = "written",
+					},
+					-- opts = { skip = true },
+					view = "mini",
+				},
+				{
+					filter = {
+						event = "msg_show",
+						find = "Already at newest change",
+					},
+					view = "mini",
+				},
+				{
+					filter = {
+						event = "exit Nvim",
+						find = "Already at newest change",
+					},
+					opts = { skip = true },
+				},
+			},
 		},
 	},
 }
