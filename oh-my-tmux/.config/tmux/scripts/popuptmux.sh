85c12896d62a20055d905d7f8d96814ce02c8862
Chris <fleetscut@gmail.com>
2024-11-20 22:00:24 -0500

    fix tmux popups

--- oh-my-tmux/.config/tmux/scripts/popuptmux.sh
+++ oh-my-tmux/.config/tmux/scripts/popuptmux.sh
@@ -1,9 +1,9 @@
-#!/bin/env zsh
+#!/bin/env sh
 
 CURRENT_SESSION=$(tmux display-message -p -F "#{session_name}")
 CURRENT_SESSION_PATH=$(tmux display-message -p -F "#{session_path}")
 POPUP_SESSION=${CURRENT_SESSION}_popup
-if [[ $CURRENT_SESSION =~ "popup$" ]]; then
+if [[ $CURRENT_SESSION =~ popup$ ]]; then
     tmux detach-client
 else
     tmux popup -E "tmux attach -t $POPUP_SESSION || tmux new -s $POPUP_SESSION -c $CURRENT_SESSION_PATH"
