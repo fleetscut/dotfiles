6e37241d1972d09fd2ea1502cb09b75e55e5eadb
Chris <fleetscut@gmail.com>
2025-10-05 20:17:27 -0400

    tmux doesnt belong here

--- tmux/.config/tmux/scripts/scrollback-vim.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCROLLBACK_FILE=$(mktemp)
-SCROLLBACK_SIZE=32768
-SCROLLBACK_NAME="edit"
-
-tmux capture-pane -pS $SCROLLBACK_SIZE > $SCROLLBACK_FILE
-tmux new-window -n $SCROLLBACK_NAME "nvim '+ normal G' $SCROLLBACK_FILE"
